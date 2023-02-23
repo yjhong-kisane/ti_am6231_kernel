5023ed6c52954acffe6cd75b3d09c074e0bfcc2f
hong.yongje@kisane.com <hong.yongje@kisane.com>
2023-02-23 19:34:56 +0900

    image, modules 컴파일 완료

---
+++ linux-5.10.153+gitAUTOINC+3eee621d16-g3eee621d16/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 5 */
+/* SMP */
+#define UTS_MACHINE "aarch64"
+#define UTS_VERSION "#5 SMP Thu Feb 23 19:04:55 KST 2023"
+#define LINUX_COMPILE_BY "root"
+#define LINUX_COMPILE_HOST "ubuntu18"
+#define LINUX_COMPILER "aarch64-none-linux-gnu-gcc (GNU Toolchain for the A-profile Architecture 9.2-2019.12 (arm-9.10)) 9.2.1 20191025, GNU ld (GNU Toolchain for the A-profile Architecture 9.2-2019.12 (arm-9.10)) 2.33.1.20191209"
---
+++ linux-5.10.153+gitAUTOINC+3eee621d16-g3eee621d16/kernel/bounds.s
@@ -0,0 +1,136 @@
+	.arch armv8-a
+	.file	"bounds.c"
+// GNU C89 (GNU Toolchain for the A-profile Architecture 9.2-2019.12 (arm-9.10)) version 9.2.1 20191025 (aarch64-none-linux-gnu)
+//	compiled by GNU C version 4.8.1, GMP version 4.3.2, MPFR version 3.1.6, MPC version 1.0.3, isl version isl-0.15-1-g835ea3a-GMP
+
+// GGC heuristics: --param ggc-min-expand=100 --param ggc-min-heapsize=131072
+// options passed:  -nostdinc -I ./arch/arm64/include
+// -I ./arch/arm64/include/generated -I ./include
+// -I ./arch/arm64/include/uapi -I ./arch/arm64/include/generated/uapi
+// -I ./include/uapi -I ./include/generated/uapi
+// -iprefix /home/yjhong/kisan/TI_AM6231/toolchain/gcc-arm-9.2-2019.12-x86_64-aarch64-none-linux-gnu/bin/../lib/gcc/aarch64-none-linux-gnu/9.2.1/
+// -isysroot /home/yjhong/kisan/TI_AM6231/toolchain/gcc-arm-9.2-2019.12-x86_64-aarch64-none-linux-gnu/bin/../aarch64-none-linux-gnu/libc
+// -D __KERNEL__ -D CC_USING_PATCHABLE_FUNCTION_ENTRY
+// -D KASAN_SHADOW_SCALE_SHIFT=3 -D CONFIG_CC_HAS_K_CONSTRAINT=1
+// -D ARM64_ASM_ARCH="armv8.4-a" -D KASAN_SHADOW_SCALE_SHIFT=3
+// -D KBUILD_MODFILE="./bounds" -D KBUILD_BASENAME="bounds"
+// -D KBUILD_MODNAME="bounds"
+// -isystem /home/yjhong/kisan/TI_AM6231/toolchain/gcc-arm-9.2-2019.12-x86_64-aarch64-none-linux-gnu/bin/../lib/gcc/aarch64-none-linux-gnu/9.2.1/include
+// -include ./include/linux/kconfig.h
+// -include ./include/linux/compiler_types.h -MMD kernel/.bounds.s.d
+// kernel/bounds.c -mlittle-endian -mgeneral-regs-only -mabi=lp64
+// -mbranch-protection=pac-ret+leaf -auxbase-strip kernel/bounds.s -O2
+// -Wall -Wundef -Werror=strict-prototypes -Wno-trigraphs
+// -Werror=implicit-function-declaration -Werror=implicit-int
+// -Werror=return-type -Wno-format-security -Wno-psabi -Wno-frame-address
+// -Wformat-truncation=0 -Wformat-overflow=0 -Wno-address-of-packed-member
+// -Wframe-larger-than=1024 -Wimplicit-fallthrough=3
+// -Wno-unused-but-set-variable -Wunused-const-variable=0
+// -Wdeclaration-after-statement -Wvla -Wno-pointer-sign
+// -Wno-stringop-truncation -Wno-array-bounds -Wstringop-overflow=0
+// -Wno-restrict -Wno-maybe-uninitialized -Werror=date-time
+// -Werror=incompatible-pointer-types -Werror=designated-init
+// -Wno-packed-not-aligned -std=gnu90 -fmacro-prefix-map=./=
+// -fno-strict-aliasing -fno-common -fshort-wchar -fno-PIE
+// -fno-asynchronous-unwind-tables -fno-unwind-tables
+// -fno-delete-null-pointer-checks -fstack-protector-strong
+// -fno-omit-frame-pointer -fno-optimize-sibling-calls
+// -fpatchable-function-entry=2 -fno-strict-overflow -fstack-check=no
+// -fconserve-stack -fverbose-asm --param allow-store-data-races=0
+// options enabled:  -faggressive-loop-optimizations -falign-functions
+// -falign-jumps -falign-labels -falign-loops -fassume-phsa -fauto-inc-dec
+// -fbranch-count-reg -fcaller-saves -fcode-hoisting
+// -fcombine-stack-adjustments -fcompare-elim -fcprop-registers
+// -fcrossjumping -fcse-follow-jumps -fdefer-pop -fdevirtualize
+// -fdevirtualize-speculatively -fdwarf2-cfi-asm -fearly-inlining
+// -feliminate-unused-debug-types -fexpensive-optimizations
+// -fforward-propagate -ffp-int-builtin-inexact -ffunction-cse -fgcse
+// -fgcse-lm -fgnu-runtime -fgnu-unique -fguess-branch-probability
+// -fhoist-adjacent-loads -fident -fif-conversion -fif-conversion2
+// -findirect-inlining -finline -finline-atomics
+// -finline-functions-called-once -finline-small-functions -fipa-bit-cp
+// -fipa-cp -fipa-icf -fipa-icf-functions -fipa-icf-variables -fipa-profile
+// -fipa-pure-const -fipa-reference -fipa-reference-addressable -fipa-sra
+// -fipa-stack-alignment -fipa-vrp -fira-hoist-pressure
+// -fira-share-save-slots -fira-share-spill-slots
+// -fisolate-erroneous-paths-dereference -fivopts -fkeep-static-consts
+// -fleading-underscore -flifetime-dse -flra-remat -flto-odr-type-merging
+// -fmath-errno -fmerge-constants -fmerge-debug-strings
+// -fmove-loop-invariants -fomit-frame-pointer -foptimize-strlen
+// -fpartial-inlining -fpeephole -fpeephole2 -fplt -fprefetch-loop-arrays
+// -free -freg-struct-return -freorder-blocks -freorder-functions
+// -frerun-cse-after-loop -fsched-critical-path-heuristic
+// -fsched-dep-count-heuristic -fsched-group-heuristic -fsched-interblock
+// -fsched-last-insn-heuristic -fsched-pressure -fsched-rank-heuristic
+// -fsched-spec -fsched-spec-insn-heuristic -fsched-stalled-insns-dep
+// -fschedule-fusion -fschedule-insns -fschedule-insns2 -fsection-anchors
+// -fsemantic-interposition -fshow-column -fshrink-wrap
+// -fshrink-wrap-separate -fsigned-zeros -fsplit-ivs-in-unroller
+// -fsplit-wide-types -fssa-backprop -fssa-phiopt -fstack-protector-strong
+// -fstdarg-opt -fstore-merging -fstrict-volatile-bitfields -fsync-libcalls
+// -fthread-jumps -ftoplevel-reorder -ftrapping-math -ftree-bit-ccp
+// -ftree-builtin-call-dce -ftree-ccp -ftree-ch -ftree-coalesce-vars
+// -ftree-copy-prop -ftree-cselim -ftree-dce -ftree-dominator-opts
+// -ftree-dse -ftree-forwprop -ftree-fre -ftree-loop-if-convert
+// -ftree-loop-im -ftree-loop-ivcanon -ftree-loop-optimize
+// -ftree-parallelize-loops= -ftree-phiprop -ftree-pre -ftree-pta
+// -ftree-reassoc -ftree-scev-cprop -ftree-sink -ftree-slsr -ftree-sra
+// -ftree-switch-conversion -ftree-tail-merge -ftree-ter -ftree-vrp
+// -funit-at-a-time -fverbose-asm -fwrapv -fwrapv-pointer
+// -fzero-initialized-in-bss -mfix-cortex-a53-835769
+// -mfix-cortex-a53-843419 -mgeneral-regs-only -mglibc -mlittle-endian
+// -momit-leaf-frame-pointer -mpc-relative-literal-loads
+
+	.text
+	.section	.text.startup,"ax",@progbits
+	.align	2
+	.p2align 3,,7
+	.global	main
+	.type	main, %function
+main:
+	.section	__patchable_function_entries,"aw",@progbits
+	.8byte	.LPFE1
+	.section	.text.startup
+.LPFE1:
+	nop
+	nop
+	hint	25 // paciasp
+// kernel/bounds.c:19: 	DEFINE(NR_PAGEFLAGS, __NR_PAGEFLAGS);
+#APP
+// 19 "kernel/bounds.c" 1
+	
+.ascii "->NR_PAGEFLAGS 26 __NR_PAGEFLAGS"	//
+// 0 "" 2
+// kernel/bounds.c:20: 	DEFINE(MAX_NR_ZONES, __MAX_NR_ZONES);
+// 20 "kernel/bounds.c" 1
+	
+.ascii "->MAX_NR_ZONES 5 __MAX_NR_ZONES"	//
+// 0 "" 2
+// kernel/bounds.c:22: 	DEFINE(NR_CPUS_BITS, ilog2(CONFIG_NR_CPUS));
+// 22 "kernel/bounds.c" 1
+	
+.ascii "->NR_CPUS_BITS 8 ilog2(CONFIG_NR_CPUS)"	//
+// 0 "" 2
+// kernel/bounds.c:24: 	DEFINE(SPINLOCK_SIZE, sizeof(spinlock_t));
+// 24 "kernel/bounds.c" 1
+	
+.ascii "->SPINLOCK_SIZE 4 sizeof(spinlock_t)"	//
+// 0 "" 2
+// kernel/bounds.c:28: }
+#NO_APP
+	mov	w0, 0	//,
+	hint	29 // autiasp
+	ret	
+	.size	main, .-main
+	.ident	"GCC: (GNU Toolchain for the A-profile Architecture 9.2-2019.12 (arm-9.10)) 9.2.1 20191025"
+	.section	.note.GNU-stack,"",@progbits
+	.section	.note.gnu.property,"a"
+	.align	3
+	.word	4
+	.word	16
+	.word	5
+	.string	"GNU"
+	.word	3221225472
+	.word	4
+	.word	2
+	.align	3
