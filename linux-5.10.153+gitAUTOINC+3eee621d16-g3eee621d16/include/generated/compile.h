f8acc594113a7a942f675da957e41abaa3af20c6
hong.yongje@kisane.com <hong.yongje@kisane.com>
2023-03-07 09:49:24 +0900

    커널 설정 변경 (CONFIG_UEVENT_HELPER 추가) - /proc/sys/kernel/hotplug 없는 문제 수정
    /proc/sys/kernel/hotplug 없는 문제 수정

--- linux-5.10.153+gitAUTOINC+3eee621d16-g3eee621d16/include/generated/compile.h
+++ linux-5.10.153+gitAUTOINC+3eee621d16-g3eee621d16/include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 1 */
+/* This file is auto generated, version 2 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "aarch64"
-#define UTS_VERSION "#1 SMP PREEMPT Tue Feb 28 15:00:23 KST 2023"
+#define UTS_VERSION "#2 SMP PREEMPT Tue Mar 7 09:45:36 KST 2023"
 #define LINUX_COMPILE_BY "root"
 #define LINUX_COMPILE_HOST "ubuntu18"
 #define LINUX_COMPILER "aarch64-none-linux-gnu-gcc (GNU Toolchain for the A-profile Architecture 9.2-2019.12 (arm-9.10)) 9.2.1 20191025, GNU ld (GNU Toolchain for the A-profile Architecture 9.2-2019.12 (arm-9.10)) 2.33.1.20191209"
