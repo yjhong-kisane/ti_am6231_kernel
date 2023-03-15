9728f197c765ba4cbbf529ed42e5878b5eaad411
hong.yongje@kisane.com <hong.yongje@kisane.com>
2023-03-15 19:59:52 +0900

    TI TPS65219 관련 설정 추가

--- linux-5.10.153+gitAUTOINC+3eee621d16-g3eee621d16/include/generated/compile.h
+++ linux-5.10.153+gitAUTOINC+3eee621d16-g3eee621d16/include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 2 */
+/* This file is auto generated, version 4 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "aarch64"
-#define UTS_VERSION "#2 SMP PREEMPT Tue Mar 7 09:45:36 KST 2023"
+#define UTS_VERSION "#4 SMP PREEMPT Wed Mar 15 19:25:21 KST 2023"
 #define LINUX_COMPILE_BY "root"
 #define LINUX_COMPILE_HOST "ubuntu18"
 #define LINUX_COMPILER "aarch64-none-linux-gnu-gcc (GNU Toolchain for the A-profile Architecture 9.2-2019.12 (arm-9.10)) 9.2.1 20191025, GNU ld (GNU Toolchain for the A-profile Architecture 9.2-2019.12 (arm-9.10)) 2.33.1.20191209"
